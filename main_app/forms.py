01b7c5e98552cce6258ad866180e590713e875ab
Judy <kimjudyh@gmail.com>
2020-05-18 22:14:03 -0700

    added DateTimePickerInput widget to new lesson time field

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,11 +1,20 @@
 from django import forms 
 from .models import Lesson, Teacher
-
+from bootstrap_datepicker_plus import DateTimePickerInput
 
 class LessonForm(forms.ModelForm):
+    # time = forms.DateField(widget = DateTimePickerInput())
     class Meta:
         model = Lesson
         fields = ['name', 'description', 'price', 'time', 'level']
+        widgets = {
+            'time': DateTimePickerInput(
+                options={
+                    'format': 'MM/DD/YY hh:mm',
+                    }), 
+            
+        }
+
 
 
 class TeacherForm(forms.ModelForm):
